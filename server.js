67bd91a0376634c8b84f828920c456bc9a9ca957
git-ebetts <ebettsmail@gmail.com>
2015-09-01 11:38:42 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,50 @@
+var express = require ('express')
+var jwt = require ("jwt-simple")
+var bcrypt = require('bcrypt')
+var User = require('./user')
+var app = express()
+app.use(require("body-parser").json())
+
+
+var secretKey = "supersecretkey"
+
+app.post('/session', function(req, res, next) {
+	var username = req.body.username
+	User.findOne({username: username})
+	.select('password')
+	.exec(function (err, user){
+		if (err) {return next (err)}
+			if (!user) {return res.send(401)}
+				bcrypt.compare (req.body.password, user.password, function (err, valid){
+					if (err) {return next (err)}
+						if (!valid) {return res.send(401)}
+							var token = jwt.encode({username: user.username}, secretKey)
+						res.json(token)
+					})
+		})
+})
+
+app.get('/user', function (req, res, next) {
+	var token = req.headers['x-auth']
+	var auth = jwt.decode(token, secretKey)
+	User.findOne({usernme:auth.username}, function(err, user){
+		if (err) {return next (err)}
+		if (!user) {return res.sendStatus(401)}
+		res.json(user)
+	})
+})
+
+
+app.post('/user', function (req,res, next){
+	var user = new User({username:req.body.username})
+	bcrypt.hash(req.body.password, 10, function (err, hash) {
+		user.password = hash
+		user.save(function (err){
+			if (err) {next (err)}
+				res.sendStatus(201)
+		})
+	})
+})
+
+
+app.listen(3000)
