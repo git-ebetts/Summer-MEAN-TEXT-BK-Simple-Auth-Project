809f612a4dc967b7c29bcb28aa05b69625d85c5d
git-ebetts <ebettsmail@gmail.com>
2016-03-11 17:40:02 -0500

    Auth chapter 7 redo

--- server.js
+++ server.js
@@ -1,50 +1,55 @@
-var express = require ('express')
-var jwt = require ("jwt-simple")
+
+var express = require('express')
+var jwt = require('jwt-simple')
 var bcrypt = require('bcrypt')
 var User = require('./user')
+
 var app = express()
-app.use(require("body-parser").json())
+app.use(require('body-parser').json())
 
 
-var secretKey = "supersecretkey"
+var secretKey = 'supersecretkey'
 
-app.post('/session', function(req, res, next) {
-	var username = req.body.username
-	User.findOne({username: username})
+
+app.post('/session', function (req, res, next) {
+	  var username = req.body.username
+	User.findOne({username:username})
 	.select('password')
-	.exec(function (err, user){
-		if (err) {return next (err)}
-			if (!user) {return res.send(401)}
-				bcrypt.compare (req.body.password, user.password, function (err, valid){
-					if (err) {return next (err)}
-						if (!valid) {return res.send(401)}
-							var token = jwt.encode({username: user.username}, secretKey)
-						res.json(token)
-					})
+	.exec (function (err, user) {
+		if (err) {return next(err)}
+			if (!user) {return res.sendStatus(401)}
+				bcrypt.compare(req.body.password, 
+					user.password, function (err, valid) {
+		if (err) {return next(err)}
+			if (!valid) {return res.sendStatus(401)}
+				var token = jwt.encode({username:username}, secretKey)
+			res.json(token)
+				})
 		})
 })
 
 app.get('/user', function (req, res, next) {
 	var token = req.headers['x-auth']
 	var auth = jwt.decode(token, secretKey)
-	User.findOne({usernme:auth.username}, function(err, user){
-		if (err) {return next (err)}
+	User.findOne({username:auth.username},
+		function (err,user) {
+		if (err) {return next(err)}
 		if (!user) {return res.sendStatus(401)}
-		res.json(user)
-	})
-})
-
-
-app.post('/user', function (req,res, next){
-	var user = new User({username:req.body.username})
-	bcrypt.hash(req.body.password, 10, function (err, hash) {
-		user.password = hash
-		user.save(function (err){
-			if (err) {next (err)}
-				res.sendStatus(201)
+			res.json(user)
 		})
 	})
-})
 
+
+app.post('/user', function (req, res, next) {
+	var user = new User({username:req.body.username})
+	bcrypt.hash(req.body.password, 10, 
+		function (err, hash) {
+			user.password = hash
+			user.save(function (err,user){
+				if (err) { next(err) }
+					res.sendStatus(201)
+			})
+		})
+})
 
 app.listen(3000)
--- user.js
+++ user.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/auth_demo')
+mongoose.connect('mongodb://locahost/auth_demo')
+
 var user = mongoose.Schema({
-	username: String,
-	password: {type: String, select: false}
+  username: {type: String},
+  password: {type: String, select: false}
 })
+
 module.exports = mongoose.model('User', user)
